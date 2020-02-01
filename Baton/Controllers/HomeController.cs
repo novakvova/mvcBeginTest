b407bb920cea82331ed0d5122d8af279b41b5875
novakvova <novakvova@gmail.com>
2020-02-01 16:03:20 +0200

    Add Layout

--- Baton/Controllers/HomeController.cs
+++ Baton/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Baton.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,11 +12,26 @@ namespace Baton.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<ProductModel> model = new List<ProductModel>()
+            {
+                new ProductModel
+                {
+                    Id=1,
+                    Name="Бородінський хліб",
+                    Image="https://aerodecor.com.ua/images/%D0%91%D0%BE%D1%80%D0%BE%D0%B4%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D1%85%D0%BB%D0%B5%D0%B11_tn.jpg"
+                },
+                new ProductModel
+                {
+                    Id=2,
+                    Name="Бородінський хліб",
+                    Image="https://aerodecor.com.ua/images/%D0%91%D0%BE%D1%80%D0%BE%D0%B4%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9-%D1%85%D0%BB%D0%B5%D0%B11_tn.jpg"
+                }
+            };
+            return View(model);
         }
-        public string About()
+        public ActionResult About()
         {
-            return "My name is Vova";
+            return View();
         }
     }
 }
